c22ddb6956197a8c871cb2adf620e0e8dd886427
ItayMadeIt <itayna73@gmail.com>
2025-07-17 19:14:37 +0300

    Example for quack code

---
+++ examples/stdio.h
@@ -0,0 +1,6 @@
+extern void* malloc(unsigned long size);
+extern void free(void* ptr);
+
+extern long strlen(const char* str);
+extern int printf(const char* format, ...);
+extern void* memcpy(void *dest, const void *src, unsigned long n);
